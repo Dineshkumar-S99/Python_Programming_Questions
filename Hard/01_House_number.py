bfd63ad27a31e4d195ecaa1481038a42518a59de
Dineshkumar Subramanian <todineshkumar@outlook.com>
2023-06-24 23:20:20 +0530

    Added Greater than neighbour problem

--- Hard/01_House_number.py
+++ Hard/01_House_number.py
@@ -1,11 +1,32 @@
-T=int(input())
+'''T=int(input())
 while T>0:
     n=int(input())
     digits=""
     for i in range(1,n+1):
         digits+=str(i)
     print(len(digits))
-    T-=1
+    T-=1'''
 
 
 #above one has time complexity another solution could be!
+'''T=int(input())
+while T>0:
+    n = int(input())
+    digits = 0
+    for i in range(1, n + 1):
+        digits += len(str(i))
+    print(digits)
+    T-=1'''
+
+
+def finddigits(N):
+  num_of_digits=0
+  for i in range(1,N,10):
+    num_of_digits+=(N-i+1)
+  return num_of_digits
+
+T=int(input())
+while T>0:
+  N=int(input())
+  print((finddigits(N))+1)
+  T=T-1
---
+++ Hard/02_Greater_than_neighbour.py
@@ -0,0 +1,75 @@
+#GREATER THAN NEIGHBOUR
+
+'''
+Rahul lives in a colony, where N houses are built in a single row numbered from 0 to N-1. 
+The first house has the house number 0, the second house has house number 1 and so on.
+your friend wants to know the house number of the houses that are paying more rent than both the neighbors in the row where Rahul lives.
+First and the last house will have only one neighbor.
+
+Input format
+First line contains test case T.
+T test cases follow,
+First line contains N representing the number of houses.
+Second line contains the rent of the N houses.
+
+Output format
+For each test case, print house numbers(0-based indexing) that satisfy the above-mentioned condition.
+If no house satisfies the criterion print -1.
+
+Constraints
+1<=T<=10
+2<=N<=100
+1<=A[i]<=1000
+
+Time limit
+1 second
+
+Example
+Input
+2
+
+5
+4 3 5 2 1
+
+7
+7 2 8 4 3 9 1
+
+Output
+0 2
+0 2 5
+
+Sample test case explanation
+N=5
+Array = [4,3,5,2,1]
+
+4 is greater than 3 so print 0(house number of the house paying rent=4)
+3 is not greater than its neighbor 4 and 5 so skip it.
+5 is greater than both 3 and 2 so print 2(house number of the house paying rent=5)
+2 is greater than 1 but smaller than 5 so skip it
+1 is smaller than 2 so skip it.
+Final answer = 0 2'''
+
+
+T=int(input())
+while T>0:
+  N=int(input())
+  arr=list(map(int,input().split()))
+  if N==len(arr):
+    result=[]
+    for i in range(N):
+      if i==0:
+        if arr[i] > arr[i+1]:
+          result.append(i)
+      elif i==(N-1):
+        if arr[i] > arr[i-1]:
+          result.append(i)
+      else:
+        if arr[i] > arr[i-1] and arr[i] > arr[i-1]:
+          result.append(i)
+    if len(result)==0:
+      print(-1)
+    else:
+      for i in result:
+        print(i,end=" ")
+    print()
+  T-=1
