26d58b61710316be5fe12ec42abc0d818c58dc8a
Dineshkumar Subramanian <todineshkumar@outlook.com>
2023-07-04 23:44:59 +0530

    added basic problem 02

---
+++ basic/02_print_character.py
@@ -0,0 +1,25 @@
+#Print character
+
+'''
+You are given a character ch. You have to print the character ch same as it is.
+Note:- Character ch will contain only lowercase English letters.
+
+Input Format
+The first line of input contains a character ch.
+
+Output Format
+Print the character ch same as it is.
+
+Constraints
+a<=ch<=z
+
+Time Limit 0.5 second
+
+Example
+Sample Input
+a
+
+Sample Output
+a'''
+ch=input()
+print(ch)
