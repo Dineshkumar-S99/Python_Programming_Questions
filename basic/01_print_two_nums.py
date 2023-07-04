b5d37853196a32d855786386e0a2a076bba775b8
Dineshkumar Subramanian <todineshkumar@outlook.com>
2023-07-04 23:44:19 +0530

    added basic problem 01

---
+++ basic/01_print_two_nums.py
@@ -0,0 +1,25 @@
+#Print two numbers
+
+'''
+You are given two positive integers N and M. You have to print the number N and M separated by a space.
+
+Input Format
+The first line of input contains two integer N and M.
+
+Output Format
+Print the numbers N and M separated by a space.
+
+Constraints
+0<=N,M<=100
+
+Time Limit 0.5 second
+
+Example
+Sample Input
+5 5
+
+Sample Output
+5 5'''
+
+N,M=map(int,input().split())
+print(N,M)
