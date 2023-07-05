a110a59657b7756dbf678d7c2f9040b3c5250207
Dineshkumar Subramanian <todineshkumar@outlook.com>
2023-07-05 18:30:17 +0530

    added basic problem 07

---
+++ basic/07_reminder_of_10.py
@@ -0,0 +1,27 @@
+'''
+your friend gave you a number x, and asked you to find the remainder of x divided by 10.
+
+Input format
+A single integer x is provided.
+
+Output format
+Print a single integer representing the required output.
+
+Constraints
+1<=x<=2000
+
+Time Limit
+1 second
+
+Example
+Input
+1345
+
+Output
+5
+
+Sample Test Case Explanation
+1345/10=134 with remainder as 5.'''
+
+n=int(input())
+print(n%10)
